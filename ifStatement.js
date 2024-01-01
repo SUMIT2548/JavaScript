2795bf6390a9350d8d38bc92adbcefdbc4939d78
SUMIT2548 <sumitsona35@gmail.com>
2024-01-01 23:07:56 +0530

    js

---
+++ ifStatement.js
@@ -0,0 +1,24 @@
+//type -1
+
+let mode1 = "dark-mode";
+let color1;
+if(mode1 === "dark-mode")
+{
+    color1 = "black";
+}
+console.log(color1); 
+
+
+// type - 2 
+
+let mode = "light-mode";
+let color;
+if(mode === "dark-mode")
+{
+    color ="black";
+}
+if (mode === "light-mode")
+{
+    color = "White";
+}
+console.log(color);
---
+++ ifelseStatement.js
@@ -0,0 +1,24 @@
+/*
+let mode = "dark-mode";
+let color ;
+if(mode === "light")
+{
+    color = "black";
+}
+else
+{
+    color ="white";
+}
+console.log(color);  */ // ans - white
+
+let mode = "dark-mode";
+let color ;
+if(mode === "dark-mode")
+{
+    color = "black";
+}
+else
+{
+    color ="white";
+}
+console.log(color); // ans - black
