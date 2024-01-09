70e3d4cc8b2ff00b948f7ab5fcdce1c5c3df4af0
SUMIT2548 <sumitsona35@gmail.com>
2024-01-09 23:04:00 +0530

    js

--- Dom/dom.js
+++ Dom/dom.js
@@ -1,1 +1,24 @@
 alert("Hello!");
+console.log("sumit");
+
+
+console.log(document.body);
+console.dir(document.body);
+
+// DOM Manipulation
+
+let head = document.getElementById("Heading");
+console.log(head);
+let Name = document.getElementsByClassName("ClassName");
+console.log(Name);
+let t = document.getElementsByTagName("p");
+console.log(t);
+
+
+let ta = document.querySelector("p");
+console.log(ta);  // print the first element p tag not all
+//console.dir(ta); 
+
+let ta1 = document.querySelectorAll("p");
+console.log(ta1);
+console.dir(ta1);
